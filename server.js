ddd8b96c39e5e9ccb1719c74469d083345609073
Kassim <kassimhuggins@macbook-pro.attlocal.net>
2021-04-14 18:58:21 -0700

    new department and display menu

--- server.js
+++ server.js
@@ -3,14 +3,25 @@ require('dotenv').config();
 
 const inquirer = require('inquirer');
 
-const db = require('./db/database')
+const db = require('./db/database');
 
-const { getAlldepartments, addDepartment} = require('./utils/departments');
+const { getAllDepartments, addDepartment} = require('./utils/departments');
 const { getAllRoles, addRole } = require('./utils/roles');
 
-const { getAllEmployees, addEmployee } = require('./utils/employees');
+const { getAllEmployees, addEmployee, updateRole} = require('./utils/employees');
 
+const displayMenu = require('./utils/displayMenu');
 
 const { NULL } = require('mysql2/lib/constants/types');
 
-addEmployee('Mark', 'Chad', 8);
+// addEmployee('Mark', 'Chad', 8);
+
+// updateRole();
+
+displayMenu()
+.then(() => {
+    console.log('\n');
+})
+.catch(err => {
+    console.log(err);
+});
