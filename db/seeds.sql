5a9ac158f356a79d2fed055c336b308cf97aa740
Kassim <kassimhuggins@macbook-pro.attlocal.net>
2021-04-16 20:32:18 -0700

    reroot

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
+use employee_tracker;
+
 INSERT INTO departments (name)
 VALUES
 ('Sales'),
@@ -10,26 +12,20 @@ VALUES
 
 INSERT INTO roles(title, salary, department_id)
 VALUES
-('Sales Lead Team', 2000, 4),
-('Salesperson', 4000, 2),
-('Lead Engineer', 80000, 3),
- ('Accountant',6000,4),
- ('Bookeeper',20000,1),
- ('Lawyer',80000, 2),
- ('Legal Team Lead',50000,1);
+ ('Sales Lead Team', 100000,1),
+ ('Salesperson', 80000,1),
+ ('Sofware Engineer', 120000, 2), 
+ ('Lead Engineer', 130000, 2),
+ ('Accountant',125000,3),
+ ('Bookeeper',100000,3),
 
  INSERT INTO employees (first_name, last_name, manager_id, role_id)
 VALUES 
-  ('Becky', 'Cage', NULL, 1),
-  ('Mark', 'Force', 1, 2),
-  ('Michael', 'Scott', NULL, 4),
-  ('Scott', 'Pilgram', 3, 3),
-  ('Melissa', 'Mccarthy', 3, 3),
-  ('Brad', 'Pitt', NULL, 5),
-  ('Henry', 'Ford', 6, 6),
-  ('Michael', 'Jackson', 6, 6),
-  ('Thomas', 'Hill', 10, 7),
-  ('Ned', 'Sanders', NULL, 8),
-  ('Owen', 'Wilson', 10, 7),
-  ('Karl', 'Malone', 3, 3),
-  ('Kobe', 'Bryant', 3, 3); 
+  ('Anthony', 'Trollope',1, 1),
+  ('Charlotte', 'Yonge', 2, 2),
+  ('Horace', 'Walpole', 3, 3),
+  ('Matthew', 'Lewis', 4, 4),
+  ('William', 'Bedford', 5, 5),
+  ('Anne', 'Radcliffe', 6,6),
+  ('Charles', 'Brown', 7, 7),
+ 
